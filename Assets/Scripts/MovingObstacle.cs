7fd45255e387c17ad95876a6ccf6eaadef25130d
ellpei <loulou.pei@hotmail.com>
2018-12-19 18:03:39 +0100

    moving barrier on lvl 1

---
+++ Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*Script for creating moving obstacles*/
+public class MovingObstacle : MonoBehaviour {
+
+    public float lowerBound;
+    public float upperBound;
+    private bool movingUp = false;
+    float moveSpeed = 0.15f;
+
+	// Use this for initialization
+	void Start () {
+
+        lowerBound = -Camera.main.orthographicSize;
+        upperBound = Camera.main.orthographicSize;
+        transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(transform.position.y <= lowerBound)
+        {
+            movingUp = true;
+        } else if(transform.position.y >= upperBound)
+        {
+            movingUp = false;
+        }
+
+        if(!movingUp)
+        {
+            //move downwards 
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed, transform.position.z);
+        }
+        else if(movingUp)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed, transform.position.z);
+        }
+
+    }
+}
